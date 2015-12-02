8db66fddee91d1356cc0898beed7e925ef8641ab
Oopsno <yufeiliu94@gmail.com>
2015-12-02 13:17:45 +0800

    node_type_t移入kaleido::Node

--- lang/include/node/basic.hpp
+++ lang/include/node/basic.hpp
@@ -6,30 +6,13 @@
 
 namespace kaleido {
 
-typedef enum {
-  undef, identifier, aexp, bexp, loop, assign, type, decl,
-  call, mod_import, mod_decl, block, float_literal
-} node_type_t;
-
-//constexpr operator const char *(const node_type_t);
-
-template<typename tp>
-struct auto_node_type {
-  const static node_type_t value = undef;
-};
-
-template<>
-struct auto_node_type<std::string> {
-  const static node_type_t value = identifier;
-};
-
-template<>
-struct auto_node_type<double> {
-  const static node_type_t value = float_literal;
-};
-
 class Node {
  public:
+  typedef enum {
+    undef, identifier, aexp, bexp, loop, assign, type, decl,
+    call, mod_import, mod_decl, block, float_literal
+  } node_type_t;
+
   node_type_t node_type;
   Node();
   Node(const node_type_t type);
