945789f84e605ee4e54546cacc41388b46e8ccf4
Oopsno <yufeiliu94@gmail.com>
2015-12-07 17:22:23 +0800

    Makes now

--- lang/test/ast.cpp
+++ lang/test/ast.cpp
@@ -1,12 +1,14 @@
 #include "ast/arithmetic.hpp"
+#include "context.hpp"
 
 using namespace kaleido::ast;
 
 int main() {
+  codegen::Context ctx;
   Immediate<int64_t> imm_i(42);
   Immediate<double> imm_f(42.0);
   imm_f.dump(0);
-  imm_f.codegen(llvm::getGlobalContext())->dump();
+  imm_f.codegen(ctx)->dump();
   imm_i.dump(0);
-  imm_i.codegen(llvm::getGlobalContext())->dump();
+  imm_i.codegen(ctx)->dump();
 }
