76b92c2ccee6354083615154c1dc7c144a38aa24
Oopsno <yufeiliu94@gmail.com>
2015-12-08 13:06:54 +0800

    using lookup table now

--- lang/src/ast/operators.cpp
+++ lang/src/ast/operators.cpp
@@ -1,63 +1,42 @@
 #include "ast/operators.hpp"
-#include <string>
 
-static const std::string add("+");
-static const std::string sub("-");
-static const std::string mul("*");
-static const std::string div("/");
-static const std::string idiv("//");
-static const std::string mod("&");
-static const std::string exp("^");
-static const std::string and_("and");
-static const std::string or_("or");
-static const std::string xor_("xor");
 static const std::string neg_("neg");
 static const std::string not_("not");
+static const std::string nz("NONZERO");
 static const std::string unknown("Unknown");
 
 namespace kaleido {
 namespace ast {
 namespace op {
 
+static const std::string bop_table[]{
+    "add", "sub", "mul", "div", "idiv", "mod", "exp",
+    "and", "or", "xor",
+    "gt", "ge", "eq", "le", "lt", "ne",
+    "undefined_binary_operator"
+};
+
+static const std::string uop_table[]{
+    "NEGATE", "NOT", "NONZERO", "UNDEFINED_UNARY_OPERATOR"
+};
+
 // Binary
 
 const std::string &name_of(BinaryOperator op) {
-  switch (op) {
-    case ADD :
-      return add;
-    case SUB :
-      return sub;
-    case MUL :
-      return mul;
-    case DIV :
-      return div;
-    case IDIV:
-      return idiv;
-    case MOD :
-      return mod;
-    case EXP :
-      return exp;
-    case AND :
-      return and_;
-    case OR :
-      return or_;
-    case XOR:
-      return xor_;
-    default:
-      return unknown;
+  if (op >= op::UNDEFINED_BINARY_OPERATOR || op < 0) {
+    return bop_table[op::UNDEFINED_BINARY_OPERATOR];
+  } else {
+    return bop_table[op];
   }
 }
 
 // Unary
 
 const std::string &name_of(UnaryOperator op) {
-  switch (op) {
-    case NOT:
-      return not_;
-    case NEGATE:
-      return neg_;
-    default:
-      return unknown;
+  if (op >= op::UNDEFINED_UNARY_OPERATOR || op < 0) {
+    return uop_table[UNDEFINED_UNARY_OPERATOR];
+  } else {
+    return uop_table[op];
   }
 }
 
