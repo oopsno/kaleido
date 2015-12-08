4f65a5317de3db3cb29f0ebf4f29385435fe2693
Oopsno <yufeiliu94@gmail.com>
2015-12-09 02:43:35 +0800

    including all headers

--- lang/include/ast.hpp
+++ lang/include/ast.hpp
@@ -1,8 +1,11 @@
 #ifndef KALEIDO_AST_HPP
 #define KALEIDO_AST_HPP
 
+#include "ast/type.hpp"
 #include "ast/ast.hpp"
 #include "ast/arithmetic.hpp"
 #include "ast/bool.hpp"
+#include "ast/statement.hpp"
+#include "ast/rval.hpp"
 
 #endif //KALEIDO_AST_HPP
