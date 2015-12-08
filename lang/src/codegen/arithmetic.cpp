9ededd471c26bf2886e6dea80dce2206bb928c58
Oopsno <yufeiliu94@gmail.com>
2015-12-08 13:07:14 +0800

    + earlier type-check

--- lang/src/codegen/arithmetic.cpp
+++ lang/src/codegen/arithmetic.cpp
@@ -3,7 +3,7 @@
 namespace kaleido {
 namespace ast {
 
-llvm::Value* VariableRef::codegen(codegen::Context &ctx) {
+llvm::Value *VariableRef::codegen(codegen::Context &ctx) {
   // TODO deal with symbol table
   return nullptr;
 }
@@ -11,6 +11,7 @@ llvm::Value* VariableRef::codegen(codegen::Context &ctx) {
 llvm::Value *BAO::codegen(codegen::Context &ctx) {
   llvm::Value *lhs_value = lhs->codegen(ctx);
   llvm::Value *rhs_value = rhs->codegen(ctx);
+  // TODO Add type check
   switch (bop) {
     case op::ADD:
       return ctx.builder.CreateFAdd(lhs_value, rhs_value);
@@ -26,10 +27,14 @@ llvm::Value *BAO::codegen(codegen::Context &ctx) {
 }
 
 llvm::Value *UAO::codegen(codegen::Context &ctx) {
-  llvm::Value *target = arithmetic->codegen(ctx);
+  llvm::Value *value = arithmetic->codegen(ctx);
   switch (uop) {
     case op::NEGATE:
-      return ctx.builder.CreateFNeg(target);
+      if (value->getType()->isIntegerTy()) {
+        return ctx.builder.CreateNeg(value);
+      } else {
+        return ctx.builder.CreateFNeg(value);
+      }
     default:
       throw std::invalid_argument("Unsupport operator: " + op::name_of(uop));
   }
